235d85d75649b3122b283a2b6e3d4d3157e5cb27
d1snin <~@d1s.uno>
2022-01-08 20:54:31 +0300

    using shadow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("maven-publish")
     id("org.springframework.boot") version "2.6.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
 }
